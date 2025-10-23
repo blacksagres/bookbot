5858a5e651e546cccf13238d4ccd40a7c05e8ac7
blacksagres <geraldo.nogueirabs@gmail.com>
2025-10-23 20:28:06 +0200

    CH:2 L6: complete

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+from stats import get_num_words, count_characters
+
+FRANKENSTEIN_BOOK_PATH = './books/frankenstein.txt'
+
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
+        return f.read()
+
+def main():
+    book_text = get_book_text(FRANKENSTEIN_BOOK_PATH)
+    word_amount = get_num_words(book_text)
+
+    print(f'Found {word_amount} total words')
+
+    character_count = count_characters(book_text)
+
+    print(character_count)
+
+
+main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_num_words(text: str):
     return len(text.split())
+
+def count_characters(text: str):
+    characters = {}
+
+    for character in text:
+        if character.lower() not in characters:
+            characters[character.lower()] = 1
+        else:
+            characters[character.lower()] += 1
+    
+    return characters
